8e6aad051609556bc55615a7bedf33211eeba1f4
Putu Pradnya <putu.pradnya@divistant.com>
2025-09-03 20:46:58 +0700

    latihan stuktur data

---
+++ 03_operator.py
@@ -0,0 +1,18 @@
+# aritmatika
+x = 10
+y = 5
+
+# operatornya
+penjumlahan = x + y
+pengurangan = x - y
+perkalian = x * y
+pembagian = x / y
+
+# print(perkalian)
+
+# operator perbandingan
+# print(y != x)
+
+# operator logika
+# print(x > 2 and y > 8)
+print(x > 2 or y > 8)
---
+++ 04_struktur_data.py
@@ -0,0 +1,43 @@
+# # List
+# hewan = ["macan", "kuda", "kura-kura", "gajah", "komodo", "cappybara"]
+
+# print(hewan)
+
+# # tambah data
+# hewan.append("burung")
+# print(hewan)
+
+# # urutan --> index:0,1,2...
+# print(hewan[1:3])
+
+# # hewan.remove("Kuda")
+# # print(hewan)
+
+# print(len(hewan))
+
+
+# dictonary
+# mobil = {
+#     "brand": ["Toyota", "Honda"],
+#     "Warna": ["Pink", "hitam"],
+#     "type": ["SUV", "Sedan"],
+#     "PlateNumber": ["BE 1 ", "BE 2"],
+#     "Tahun": [2020, 2022]
+# }
+
+# well = {
+#     "depth": [100, 200, 300],
+#     "density": [1.5, 1.6, 1.7]
+# }
+
+# print(mobil["Warna"][1])
+
+
+# tuple
+# buah = ("apel", "jeruk", "pisang")
+
+# print(buah[-1])
+
+# Set
+buah = {"apel", "jeruk", "pisang", "mangga", "apel"}
+
